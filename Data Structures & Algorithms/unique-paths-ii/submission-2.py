fcc17d74b657c057187b32cb6c0c1f67c51eafb0
Eqan Ahmad <eqan.ahmad123@gmail.com>
2026-09-12 12:48:23 +0200

    Add: unique-paths-ii - submission-2

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-2.py
@@ -0,0 +1,38 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+
+        # Top Down - Memoization Approach
+        def memoize(r, c, ROWS, COLS, cache):
+            if r == ROWS or c == COLS:
+                return 0
+            if obstacleGrid[r][c] == 1:
+                return 0
+            if cache[r][c] > 0:
+                return cache[r][c]
+            if r == ROWS - 1 and c == COLS - 1:
+                return 1
+            
+            cache[r][c] = memoize(r + 1, c, ROWS, COLS, cache) + memoize(r, c + 1, ROWS, COLS, cache)
+            return cache[r][c]
+            
+
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        # return memoize(0, 0, m, n , [[0] * n for _ in range(m) ])
+
+        # Bottom Up - True DP approach
+        def bottomUp(m, n):
+            prevRow = [0] * n
+            for r in range(m - 1, -1, -1):
+                currRow = [0] * n
+                for c in range(n - 1, -1, -1):
+                    if obstacleGrid[r][c] == 1:
+                        currRow[c] = 0
+                    elif r == m - 1 and c == n - 1:
+                        currRow[c] = 1
+                    else:
+                        down, right = prevRow[c], currRow[c + 1] if c + 1 < n else 0
+                        currRow[c] = down + right
+                prevRow = currRow
+            return prevRow[0]
+
+        return bottomUp(m, n)
