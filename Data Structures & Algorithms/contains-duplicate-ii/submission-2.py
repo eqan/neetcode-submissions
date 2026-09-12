42e39ca38b6c6ddf356efbfa428ff72081e9c7d9
Eqan Ahmad <eqan.ahmad123@gmail.com>
2026-09-12 23:09:16 +0200

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+
+        # Brute Force Method
+        def bruteForce():
+            for L in range(len(nums)): # Starting left pointer
+                for R in range(L+1, min(len(nums), L+k+1)): # Right pointer starting after L and remains inbound the sliding window range or total length of nums
+                    if nums[L] == nums[R]: # If value compares return True
+                        return True
+            return False
+
+        #return bruteForce()
+
+        def memoize():
+            window = set()
+            L = 0
+            for R in range(len(nums)):
+                if R - L > k:
+                    window.remove(nums[L])
+                    L+=1
+                if nums[R] in window:
+                    return True
+                window.add(nums[R])
+            return False
+        return memoize()
+
+
+        
