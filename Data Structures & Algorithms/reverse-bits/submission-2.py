c82e15f6ae650b9d1dbf3ebcda1a17c13adbe11e
Eqan Ahmad <eqan.ahmad123@gmail.com>
2026-09-12 21:29:50 +0200

    Add: reverse-bits - submission-2

---
+++ Data Structures & Algorithms/reverse-bits/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(32):
+            bit = (n >> i) & 1 # Using the and operator to retain only the 1's discaeding the 0s, with the right shifting it helps us discard the elements on the right side
+            res = res | (bit << (31 - i)) # Would be doing logic OR with the result and the computed result, the computed result we are doing a similar thing with the offset, now offset is and subtracting from 31(total number of bits), places the bit at its new home
+        return res
+        
